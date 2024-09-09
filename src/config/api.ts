0498ef2d1e0903c2e679298421169a58f4871470
Arman Mondal <arman.mondal.27012005@gmail.com>
2024-09-09 13:56:55 +0530

    modified:   src/config/api.ts

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 export const API_URL='https://pinterest-server-6zsk.onrender.com/';
-=======
-export const API_URL='https://pinterest-server-6zsk.onrender.com/';
->>>>>>> db1828b8a921fe75554fa1be4f653e1a41afc3fb
